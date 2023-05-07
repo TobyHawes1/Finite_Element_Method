0e0ef69cd3bb95f415a83fcad32c3df3e926a4e8
TobyHawes1 <115074952+TobyHawes1@users.noreply.github.com>
2023-05-07 11:07:47 +0100

    Delete FiniteElementMethod_Implementation_TobyHawes directory

--- FiniteElementMethod_Implementation_TobyHawes/boundaryconditions/laplace/setLaplacePeriodicBC.m
+++
@@ -1,9 +0,0 @@
-function [u] = setLaplacePeriodicBC (u,dof,domain,variable)
-
-domainVariable  = strcat(domain,'_',variable);
-periodicBCnodes = strcat(domainVariable,'_periodicBC');
-index1 = dof.(periodicBCnodes)(1,:);
-index2 = dof.(periodicBCnodes)(2,:);
-u.(domainVariable)(index2,1) = u.(domainVariable)(index1,1);
-
-end
--- FiniteElementMethod_Implementation_TobyHawes/endproblem.m
+++
@@ -1,9 +0,0 @@
-function [] = endproblem (myPath,problem)
-
-[Folder,SubFolders] = feval(myPath);
-
-problem = strcat(Folder,'data/',problem);
-
-rmpath(problem);
-
-end
